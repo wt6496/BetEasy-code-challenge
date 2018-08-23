41a2a748a42c77dc6e5f6b6ec6b2d2bf6b68ff9c
Steven.Wang <wt6495@gmail.com>
2018-08-23 23:25:33 +0800

    Implement RawRaceFeedDataProvider.CreateRawRaceFeed method.

--- dotnet-code-challenge/RawRaceFeedDataProvider.cs
+++ dotnet-code-challenge/RawRaceFeedDataProvider.cs
@@ -1,6 +1,7 @@
 ﻿using dotnet_code_challenge.Models.RaceFeeds;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace dotnet_code_challenge
@@ -9,7 +10,35 @@ namespace dotnet_code_challenge
     {
         public static RawRaceFeed CreateRawRaceFeed(string theFilePath)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(theFilePath))
+            {
+                throw new ArgumentNullException(theFilePath);
+            }
+
+            var aFullPath = string.Format(
+                "{0}{1}", 
+                AppDomain.CurrentDomain.BaseDirectory, 
+                theFilePath.TrimStart('\\'));
+
+            if (!File.Exists(aFullPath))
+            {
+                throw new FileNotFoundException(aFullPath);
+            }
+
+            var aMemoryStream = new MemoryStream();
+            using (var aFileStream = new FileStream(aFullPath, FileMode.Open, FileAccess.Read))
+            {
+                aFileStream.CopyTo(aMemoryStream);
+            }
+
+            var aFile = new FileInfo(aFullPath);
+            var aRawRaceFeed = new RawRaceFeed
+            {
+                FileName = aFile.Name,
+                FeedStream = aMemoryStream
+            };
+
+            return aRawRaceFeed;
         }
     }
 }
